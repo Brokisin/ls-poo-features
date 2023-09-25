be3ed5d3713fd63ea953ccbb21f37534a79cef13
Eros Karsburg <eroskarsburg@gmail.com>
2023-09-25 16:35:06 -0300

    feature: implemented employee class

---
+++ ls-poo-features/Entity/Employee/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace ls_poo_features.Entity.Employee
+{
+    public class Employee
+    {
+        public Employee(string? nome, string? cPF, float salario, string? departamento)
+        {
+            Nome = nome;
+            CPF = cPF;
+            Salario = salario;
+            Departamento = departamento;
+        }
+
+        public string? Nome { get; set; }
+        public string? CPF { get; set; }
+        public double Salario { get; set; }
+        public string? Departamento { get; set; }
+
+        public virtual double Bonificar()
+        {
+            return Salario + (Salario * 0.1);
+        }
+    }
+}
