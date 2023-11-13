6e4462c51d84d5117cb582dd625d399b3959602e
Eros Karsburg <eroskarsburg@gmail.com>
2023-11-13 20:02:02 -0300

    changes

---
+++ ls-poo-features/Entity/Empregado/Empregado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ls_poo_features.Entity.Empregado
+{
+    internal class Empregado
+    {
+    }
+}
